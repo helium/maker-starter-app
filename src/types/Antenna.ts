e284dbef44651aef2d236e73098695f71f92cb73
Vladimir Igonin <igoninvladimir@mail.ru>
2022-02-10 13:03:12 +0400

    Allow to choose custom antenna settings

--- src/types/Antenna.ts
+++ src/types/Antenna.ts
@@ -1,9 +1,25 @@
 export type Antenna = {
+  id: string
   name: string
   gain: number
 }
 
 export const defaultAntenna: Antenna = {
+  id: 'freedomfi',
   name: 'FreedomFi Antenna',
   gain: 1.7,
 }
+
+export const customAntenna: Antenna = {
+  id: 'custom',
+  name: 'Custom Antenna',
+  gain: 1,
+}
+
+export const antennas = [defaultAntenna, customAntenna].map((antenna) => {
+  return {
+    ...antenna,
+    label: antenna.name,
+    value: antenna.id,
+  }
+})
