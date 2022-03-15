612ad14c8a9cc532c0db3a04e0b3d1016829a7e0
Andrewsx <andrewsopoku6@gmail.com>
2022-03-15 23:57:46 +0000

    higher resolution for education img added

--- src/features/hotspots/setup/hotspotSetupSlides.ts
+++ src/features/hotspots/setup/hotspotSetupSlides.ts
@@ -4,22 +4,22 @@ const slides: Array<CarouselItemData> = [
   {
     title: 'hotspot_setup.education.cards.0.title',
     desc: 'hotspot_setup.education.cards.0.subtitle',
-    image: require('../../../assets/images/educationScreen/outside.jpg'),
+    image: require('../../../assets/images/educationScreen/outside.png'),
   },
   {
     title: 'hotspot_setup.education.cards.1.title',
     desc: 'hotspot_setup.education.cards.1.subtitle',
-    image: require('../../../assets/images/educationScreen/indoor.jpg'),
+    image: require('../../../assets/images/educationScreen/indoor.png'),
   },
   {
     title: 'hotspot_setup.education.cards.2.title',
     desc: 'hotspot_setup.education.cards.2.subtitle',
-    image: require('../../../assets/images/educationScreen/building.jpg'),
+    image: require('../../../assets/images/educationScreen/building.png'),
   },
   {
     title: 'hotspot_setup.education.cards.3.title',
     desc: 'hotspot_setup.education.cards.3.subtitle',
-    image: require('../../../assets/images/educationScreen/bugview.jpg'),
+    image: require('../../../assets/images/educationScreen/bugview.png'),
   },
 ]
 
