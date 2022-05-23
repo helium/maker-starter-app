ae7264e46e6fd637a1a375ca478d6beb3adecf62
Andrewsx <andrewsopoku6@gmail.com>
2022-05-23 12:36:59 +0100

    support tab and webview added

---
+++ src/features/support/SupportScreen.tsx
@@ -0,0 +1,56 @@
+import React, { memo, useEffect, useState } from 'react'
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
+
+import { useAnalytics } from '@segment/analytics-react-native'
+import { WebView } from 'react-native-webview'
+
+import Box from '../../components/Box'
+
+import useMount from '../../utils/useMount'
+
+import { getAddress } from '../../utils/secureAccount'
+
+const SupportScreen = () => {
+  // Set segment identity
+  const { identify } = useAnalytics()
+  const [address, setAddress] = useState<string>()
+
+  const [identified, setIdentified] = useState(false)
+
+  useMount(() => {
+    getAddress().then(setAddress)
+  })
+
+  useEffect(() => {
+    if (address && !identified) {
+      identify(address)
+
+      setIdentified(true)
+    }
+  }, [address, identified, identify])
+
+  return (
+    <Box backgroundColor="primaryBackground" flex={1}>
+      <BottomSheetModalProvider>
+        <Box
+          padding="l"
+          flex={1}
+          justifyContent="center"
+          backgroundColor="primaryBackground"
+          style={{
+            flexDirection: 'column',
+          }}
+        >
+          <WebView
+            source={{
+              uri: 'https://support.nebra.com/support/home',
+            }}
+            style={{ marginTop: 30 }}
+          />
+        </Box>
+      </BottomSheetModalProvider>
+    </Box>
+  )
+}
+
+export default memo(SupportScreen)
---
+++ src/features/support/supportTypes.tsx
@@ -0,0 +1,7 @@
+import { StackNavigationProp } from '@react-navigation/stack'
+
+export type SupportStackParamList = {
+  SupportScreen: undefined
+}
+
+export type SupportNavigationProp = StackNavigationProp<SupportStackParamList>
