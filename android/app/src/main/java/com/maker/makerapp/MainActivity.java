ee52fbd536dc8591d873ddeaf069298a41c3b9a6
Matt Reetz <matthewcarlreetz@gmail.com>
2022-12-06 17:30:56 -0600

    Upgrade react native, fix android. (#49)

--- android/app/src/main/java/com/maker/makerapp/MainActivity.java
+++ android/app/src/main/java/com/maker/makerapp/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 import android.view.WindowManager;
 import expo.modules.ReactActivityDelegateWrapper;
@@ -33,6 +32,7 @@ public class MainActivity extends ReactActivity {
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new MainActivityDelegate(this, getMainComponentName());
   }
+
   public static class MainActivityDelegate extends ReactActivityDelegate {
     public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
       super(activity, mainComponentName);
@@ -62,18 +62,5 @@ public class MainActivity extends ReactActivity {
   protected void onResume() {
     super.onResume();
     getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
-  }
-
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegateWrapper(
-     this,
-     new ReactActivityDelegate(this, getMainComponentName()) {
-       @Override
-       protected ReactRootView createRootView() {
-         return new RNGestureHandlerEnabledRootView(MainActivity.this);
-       }
-     }
-   );
   }
 }
