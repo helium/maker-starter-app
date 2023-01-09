19a16e603db49d3152d5758b84d2d13bb2374238
pritam.ghanghas@gmail.com <pritam.ghanghas@gmail.com>
2023-01-09 14:11:44 +0530

    fix: Error while accessing dashboard over IOS application #175
    * updated webview's useragent to avoid google auth error
    * updated app version number

--- src/features/hmDashboard/HmDashboardScreen.tsx
+++ src/features/hmDashboard/HmDashboardScreen.tsx
@@ -64,6 +64,7 @@ const HmDashboardScreen = () => {
             source={{
               uri: getNebraDashboardUrl(),
             }}
+            userAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
             style={{ marginTop: 70 }}
             onLoadStart={onLoadStart}
             startInLoadingState
